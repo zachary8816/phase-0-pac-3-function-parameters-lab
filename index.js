e877e6570439ba32f26d0b9bf1cab97e527ac23e
zachary <zmh012004@gmail.com>
2023-10-02 23:03:04 -0700

    complete lab

---
+++ index.js
@@ -0,0 +1,26 @@
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return `My name is ${name} and I am learning ${language}.`;
+  }
+  
+  // Test the function
+  console.log(introductionWithLanguageOptional("Ada")); // Output: My name is Ada and I am learning JavaScript.
+  console.log(introductionWithLanguageOptional("Bob", "Python")); // Output: My name is Bob and I am learning Python.
+  function introduction(name) {
+    return `My name is ${name}.`;
+  }
+  function introductionWithLanguage(name, language) {
+    return `My name is ${name} and I am learning ${language}.`;
+  }
+  function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+  }
+  function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+  }
+  function introduction(name) {
+    return `Hi, my name is ${name}.`;
+  }
+  function introductionWithLanguage(name, language) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+  }
+  
